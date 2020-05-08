3ee301d81eba8c58afd83f73280b07abcd17fe4a
Anton Polevyy <polevyy.anton@gmail.com>
2020-05-08 22:54:37 +1000

    Updates

---
+++ precache-manifest.40ce402dc80530351b35b5fa9c2cba40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58be4b3b723cfe502b7e78197e3c3f31",
+    "url": "/react-apollo-github-pages/index.html"
+  },
+  {
+    "revision": "28db9b4f8a205adc027d",
+    "url": "/react-apollo-github-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b349d9c7ff30b6ff34d",
+    "url": "/react-apollo-github-pages/static/js/2.74152bd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-apollo-github-pages/static/js/2.74152bd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28db9b4f8a205adc027d",
+    "url": "/react-apollo-github-pages/static/js/main.e362975e.chunk.js"
+  },
+  {
+    "revision": "1c687e3c886dde1e016c",
+    "url": "/react-apollo-github-pages/static/js/runtime-main.b5d4ea47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-apollo-github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
